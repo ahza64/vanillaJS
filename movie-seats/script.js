a612fceadac8f6b54032494841f063a563157c17
ahza64 <justinmichaelpettit@gmail.com>
2021-11-30 18:28:29 -0800

    localStorage seats indexes and movie price and movie price ist position

--- movie-seats/script.js
+++ movie-seats/script.js
@@ -6,9 +6,19 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value; //+ changes string to number
 
+//save movie and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('slectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //update total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -19,6 +29,7 @@ function updateSelectedCount() {
 //select movie and update
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
