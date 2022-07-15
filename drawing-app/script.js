a3bdbe485c1a066dd1e029560fbc860184be74f8
ahza64 <justinmichaelpettit@gmail.com>
2022-07-15 13:30:48 -0700

    drawing app tool box functionality

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,7 +1,13 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('#canvas')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearBtn = document.getElementById('clear')
+
 const ctx = canvas.getContext('2d')
 
-let size = 20
+let size = 10
 let color = 'black'
 let x
 let y
@@ -47,3 +53,31 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2
   ctx.stroke()
 }
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+  size += 5
+
+  if (size > 50) {
+    size = 50
+  }
+
+  updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5
+
+  if (size < 5) {
+    size = 5
+  }
+
+  updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearBtn.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
